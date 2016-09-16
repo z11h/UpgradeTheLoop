99be7dca4893c47598ea60371ddc7d2a393ded50
Zakaria Ridouh <zridouh@live.com>
2016-09-16 00:32:47 -0700

    fix jQuery path

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,5 @@
 document.getElementById('go-to-options').addEventListener(function() {
   if (chrome.runtime.openOptionsPage) {
-    // New way to open options pages, if supported (Chrome 42+).
     chrome.runtime.openOptionsPage();
   } else {
     window.open(chrome.runtime.getURL('options.html'));
